ce81f52e92e2399109620b975c2c3ca8bbe03aa4
Niall <higginni@tcd.ie>
2019-10-09 21:17:30 +0100

    09 oct2019/niall (#7)
    * added some new stuff as per brian's requests
    
    * added image for trading vision

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,16 @@
 (function($) {
   $(document).ready(function() {
     // hide .navbar first
-    $(".navbar").hide();
+    $('.navbar').hide();
 
     // fade in .navbar
     $(function() {
       $(window).scroll(function() {
         // set distance user needs to scroll before we fadeIn navbar
         if ($(this).scrollTop() > 100) {
-          $(".navbar").fadeIn();
+          $('.navbar').fadeIn();
         } else {
-          $(".navbar").fadeOut();
+          $('.navbar').fadeOut();
         }
       });
     });
